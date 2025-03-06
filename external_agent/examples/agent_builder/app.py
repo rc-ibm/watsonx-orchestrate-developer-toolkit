71340b125de087c7ba8855b7f08b9aa639bdfb00
rc-ibm <100658978+rc-ibm@users.noreply.github.com>
2025-03-06 18:32:35 +0800

    fix: log req body

--- external_agent/examples/agent_builder/app.py
+++ external_agent/examples/agent_builder/app.py
@@ -29,6 +29,8 @@ app = FastAPI()
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
 	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	req = await request.json()
+	logging.error(f"{req}")
 	logging.error(f"{request}: {exc_str}")
 	content = {'status_code': 10422, 'message': exc_str, 'data': None}
 	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
