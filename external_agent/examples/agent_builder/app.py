3eeff55d92ef868239b1b9b5dfb95f9d1247b32e
rc-ibm <100658978+rc-ibm@users.noreply.github.com>
2025-03-06 17:45:25 +0800

    refactor: log 422 unprocessable entities detail

--- external_agent/examples/agent_builder/app.py
+++ external_agent/examples/agent_builder/app.py
@@ -25,6 +25,12 @@ logger.addHandler(console_handler)
 
 app = FastAPI()
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 @app.post("/chat/completions")
 async def chat_completions(
